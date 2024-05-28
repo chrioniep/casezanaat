d44c1ec210359894694b07f7530355a252032f89
chrioniep <eponde26@gmail.com>
2024-05-28 21:59:43 +0100

    final commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -12,3 +13,34 @@ export const formatPrice = (price: number) => {
   });
   return formatter.format(price);
 };
+
+export function constructMetadata({
+  title = "CaseZanaat - custom high-quality phone cases",
+  description = "Create custom high-quality phone cases in second",
+  image = "/thumbail.png",
+  icons = "/favicon.png",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@crioni_eponde",
+    },
+    icons,
+    metadataBase: new URL("https://casezanaat.vercel.app/"),
+  };
+}
